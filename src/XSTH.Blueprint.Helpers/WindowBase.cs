24b662b8b8fc9a69f5f9133ee933bdfe35e706e2
XSilverTH <XSilverTH@proton.me>
2026-06-09 23:35:16 +0330

    refactor: use virtual properties for resource path and builder ID in WindowBase

--- src/XSTH.Blueprint.Helpers/WindowBase.cs
+++ src/XSTH.Blueprint.Helpers/WindowBase.cs
@@ -7,10 +7,14 @@ public abstract class WindowBase<T> where T : Gtk.Widget
 {
     protected readonly Builder Builder;
     public T Widget { get; }
+
+    protected virtual string ResourcePath => $"/{GetType().Namespace!.Replace('.', '/')}/{GetType().Name}.ui";
+    protected virtual string BuilderId => GetType().Name;
+
     protected WindowBase()
     {
-        Builder = Builder.NewFromResource($"/{GetType().Namespace!.Replace('.', '/')}/{GetType().Name}.ui");
-        Widget = (T)Builder.GetObject(GetType().Name)!;
+        Builder = Builder.NewFromResource(ResourcePath);
+        Widget = (T)Builder.GetObject(BuilderId)!;
         ConfigureSignals(Builder);
     }
 
